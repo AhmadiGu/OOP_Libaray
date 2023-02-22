c32b8fdc9fefa8fdef30a249fc8cc97e5df51bf5
AhmadiGu <ahmadigulalai76@gmail.com>
2023-02-22 12:35:40 +0500

    Add test for person, book, rentals,teacher

---
+++ spec/rentals_spec.rb
@@ -0,0 +1,19 @@
+require './book'
+require './person'
+require './rental'
+
+person = Person.new(24, 'Gulalai')
+book = Book.new('Live', 'Allha')
+rental = Rental.new('2023/01/15', person, book)
+
+describe Rental do
+  context 'Testing Rental Class' do
+    it 'the person should have the added rental' do
+      expect(person.rentals[0]).to eq rental
+    end
+
+    it 'the book should have the added rental' do
+        expect(book.rentals[0]).to eq rental
+    end
+  end
+end
